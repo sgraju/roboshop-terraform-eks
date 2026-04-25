10a3a41fde9471fe9853797ac6122d0578d9c02c
sgraju <sgraju.tibco@gmail.com>
2026-04-25 16:50:47 +0400

    roboshop-terraform-eks

--- 80-frontend-alb/provider.tf
+++ 80-frontend-alb/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "remote-state-86s-dev"
+    bucket = "sgrd-remote-state-dev"
     key    = "roboshop-dev-eks-alb"
     region = "us-east-1"
     use_lockfile = true
