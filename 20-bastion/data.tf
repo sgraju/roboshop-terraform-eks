28ce9b6a6f2797b59991c70cfe4b2d8141ff0af3
sgraju <sgraju.tibco@gmail.com>
2026-04-25 18:16:54 +0400

    roboshop-terraform-eks

--- 20-bastion/data.tf
+++ 20-bastion/data.tf
@@ -4,7 +4,7 @@ data "aws_ami" "joindevops" {
     
     filter {
         name   = "name"
-        values = ["redhat-9-DevOps-Practice"]
+        values = ["RHEL-9-DevOps-Practice"]
     }
 
     filter {
